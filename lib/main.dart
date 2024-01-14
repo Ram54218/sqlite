90559d3badd74cd7f769135abc11aa30132b849c
Ram54218 <140522830+Ram54218@users.noreply.github.com>
2024-01-14 10:40:37 +0530

    added the new

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Person());
+    return const MaterialApp(home: new Person());
   }
 }
